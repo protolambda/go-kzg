5f7109dc81ba96fd92769706576e1926e67acef8
protolambda <proto@protolambda.com>
2021-01-11 01:30:37 +0100

    work in progress: optimized zero polynomial computation, new recovery function

--- recover_from_samples.go
+++ recover_from_samples.go
@@ -5,157 +5,82 @@ import (
 	"math/bits"
 )
 
-//func (fs *FFTSettings) PolyQuotientRemainder(a []Big, b []Big) []Big {
 //
-//}
-//
-//func (fs *FFTSettings) FastExtendedEuclideanAlgo(a []Big, b []Big) []Big {
-//	degreeA := polyDegree(a)
-//	degreeB := polyDegree(b)
-//	if degreeA == degreeB {
-//		remainder := fs.PolyQuotientRemainder(a, b)
-//		return fs.FastExtendedEuclideanAlgo(b, remainder)
-//	} else if degreeB == 0 || degreeA > degreeB {
-//		m := fs.Mgcd(a, b)
-//
-//	} else {
-//		return fs.FastExtendedEuclideanAlgo(b, a)
+//func (fs *FFTSettings) mulPolysDirect(dst []Big, a []Big, b []Big) {
+//	if len(b) == 0 || len(a) == 0 || len(dst) != len(b) + len(a) - 1 {
+//		panic("invalid usage, inputs must not be zero length, and destination needs to match")
 //	}
-//}
-//
-//type ZeroPolyFn func(zeroVector []bool) ([]Big, []Big)
-//
-//// zeroVector input: true for 1, false for 0
-//func (fs *FFTSettings) ZeroPolynomialViaGCD(zeroVector []bool) ([]Big, []Big) {
-//	// TODO: change to in-place, and re-order steps, can alloc much less that way
-//	e1 := make([]Big, len(zeroVector), len(zeroVector))
-//	e2 := make([]Big, len(zeroVector), len(zeroVector))
-//	for i, v := range zeroVector {
-//		if !v {
-//			CopyBigNum(&e1[i], &ZERO)
-//			CopyBigNum(&e2[i], &ZERO)
+//	for i := 0; i < len(dst); i++ {
+//		CopyBigNum(&dst[i], &ZERO)
+//	}
+//	var tmp Big
+//	for i := 0; i < len(b); i++ {
+//		if equalZero(&b[i]) {
+//			continue
+//		} else if equalOne(&b[i]) {
+//			for j := 0; j < len(a); j++ {
+//				addModBig(&dst[i+j], &dst[i+j], &a[j])
+//			}
 //		} else {
-//			if i&1 == 0 {
-//				CopyBigNum(&e1[i], &ZERO)
-//				CopyBigNum(&e2[i], &ONE)
-//			} else {
-//				CopyBigNum(&e1[i], &ONE)
-//				CopyBigNum(&e2[i], &ZERO)
+//			for j := 0; j < len(a); j++ {
+//				mulModBig(&tmp, &b[i], &a[j])
+//				addModBig(&dst[i+j], &dst[i+j], &tmp)
 //			}
 //		}
 //	}
-//	p1, err := fs.FFT(e1, true)
-//	if err != nil {
-//		panic(err)
-//	}
-//	p2, err := fs.FFT(e2, true)
-//	if err != nil {
-//		panic(err)
-//	}
-//	zeroPoly := fs.FastEuclideanAlgo(p1, p2)
-//
-//	// quickly check the zero poly degree
-//	{
-//		zeroCount := 0
-//		for _, v := range zeroVector {
-//			if !v {
-//				zeroCount += 1
-//			}
-//		}
-//		degree := polyDegree(zeroPoly)
-//		if degree != zeroCount {
-//			panic(fmt.Sprintf("unexpected difference between zero poly degree %d and zero count %d", degree, zeroCount))
-//		}
-//	}
-//
-//	r, err := fs.FFT(zeroPoly, false)
-//	for i, a := range zeroVector {
-//		b := &r[i]
-//		if !((!a && equalZero(b)) || (a && !equalZero(b))) {
-//			panic(fmt.Sprintf("recovery output mismatch: index: %d, zeroVector[i]: %v, result: %d", i, a, bigStr(b)))
-//		}
-//	}
-//	return r, zeroPoly
 //}
-
-
-type ZeroPolyFn func (zeroVector []bool) ([]Big, []Big)
-
-func (fs *FFTSettings) mulPolysDirect(dst []Big, a []Big, b []Big) {
-	if len(b) == 0 || len(a) == 0 || len(dst) != len(b) + len(a) - 1 {
-		panic("invalid usage, inputs must not be zero length, and destination needs to match")
-	}
-	for i := 0; i < len(dst); i++ {
-		CopyBigNum(&dst[i], &ZERO)
-	}
-	var tmp Big
-	for i := 0; i < len(b); i++ {
-		if equalZero(&b[i]) {
-			continue
-		} else if equalOne(&b[i]) {
-			for j := 0; j < len(a); j++ {
-				addModBig(&dst[i+j], &dst[i+j], &a[j])
-			}
-		} else {
-			for j := 0; j < len(a); j++ {
-				mulModBig(&tmp, &b[i], &a[j])
-				addModBig(&dst[i+j], &dst[i+j], &tmp)
-			}
-		}
-	}
-}
-
-func (fs *FFTSettings) mulPolys(dst []Big, a []Big, b []Big) {
-	if len(a) <= 64 || len(b) <= 64 {
-		fs.mulPolysDirect(dst, a, b)
-	} else {
-		n := nextPowOf2(uint64(len(a) + len(b)))
-		stride := fs.maxWidth / n
-		out := truncatePoly(fs.mulPolysWithFFT(a, b, stride))
-		for i := 0; i < len(out); i++ {
-			CopyBigNum(&dst[i], &out[i])
-		}
-		for i := len(out); i < len(dst); i++ {
-			CopyBigNum(&dst[i], &ZERO)
-		}
-	}
-}
-
-func (fs *FFTSettings) mulManyPolys(polys... []Big) []Big {
-	n := uint64(0)
-	for _, p := range polys {
-		l := uint64(len(p))
-		n += l
-	}
-	n = nextPowOf2(n)
-	if n > fs.maxWidth {
-		panic("poly output too large")
-	}
-	prods := make([]Big, n, n)
-	for i := uint64(0); i < n; i++ {
-		CopyBigNum(&prods[i], &ONE)
-	}
-	tmp := make([]Big, n, n)
-	tmpOut := make([]Big, n, n)
-	for _, p := range polys {
-		for i := 0; i < len(p); i++ {
-			CopyBigNum(&tmp[i], &p[i])
-		}
-		for i := uint64(len(p)); i < n; i++ {
-			CopyBigNum(&tmp[i], &ZERO)
-		}
-		if err := fs.InplaceFFT(tmp, tmpOut, false); err != nil {
-			panic(err)
-		}
-		for j := uint64(0); j < n; j++ {
-			mulModBig(&prods[j], &prods[j], &tmpOut[j])
-		}
-	}
-	if err := fs.InplaceFFT(prods, tmpOut, true); err != nil {
-		panic(err)
-	}
-	return tmpOut
-}
+//
+//func (fs *FFTSettings) mulPolys(dst []Big, a []Big, b []Big) {
+//	if len(a) <= 64 || len(b) <= 64 {
+//		fs.mulPolysDirect(dst, a, b)
+//	} else {
+//		n := nextPowOf2(uint64(len(a) + len(b)))
+//		stride := fs.maxWidth / n
+//		out := truncatePoly(fs.mulPolysWithFFT(a, b, stride))
+//		for i := 0; i < len(out); i++ {
+//			CopyBigNum(&dst[i], &out[i])
+//		}
+//		for i := len(out); i < len(dst); i++ {
+//			CopyBigNum(&dst[i], &ZERO)
+//		}
+//	}
+//}
+//
+//func (fs *FFTSettings) mulManyPolys(polys... []Big) []Big {
+//	n := uint64(0)
+//	for _, p := range polys {
+//		l := uint64(len(p))
+//		n += l
+//	}
+//	n = nextPowOf2(n)
+//	if n > fs.maxWidth {
+//		panic("poly output too large")
+//	}
+//	prods := make([]Big, n, n)
+//	for i := uint64(0); i < n; i++ {
+//		CopyBigNum(&prods[i], &ONE)
+//	}
+//	tmp := make([]Big, n, n)
+//	tmpOut := make([]Big, n, n)
+//	for _, p := range polys {
+//		for i := 0; i < len(p); i++ {
+//			CopyBigNum(&tmp[i], &p[i])
+//		}
+//		for i := uint64(len(p)); i < n; i++ {
+//			CopyBigNum(&tmp[i], &ZERO)
+//		}
+//		if err := fs.InplaceFFT(tmp, tmpOut, false); err != nil {
+//			panic(err)
+//		}
+//		for j := uint64(0); j < n; j++ {
+//			mulModBig(&prods[j], &prods[j], &tmpOut[j])
+//		}
+//	}
+//	if err := fs.InplaceFFT(prods, tmpOut, true); err != nil {
+//		panic(err)
+//	}
+//	return tmpOut
+//}
 
 // if not already a power of 2, return the next power of 2
 func nextPowOf2(v uint64) uint64 {
@@ -165,40 +90,20 @@ func nextPowOf2(v uint64) uint64 {
 	return uint64(1) << bits.Len64(v-1)
 }
 
-func (fs *FFTSettings) ZeroPolyViaMultiplication(zeroVector []bool) ([]Big, []Big) {
-	//n := nextPowOf2(uint64(len(zeroVector)))
-	//domainStride := fs.maxWidth / n
-
-	//ps := [][]Big{make([]Big, 0, n / 64)}
-	//
-	//for i, x := range zeroVector {
-	//	if !x {
-	//		term := []Big{ZERO, ONE}
-	//		subModBig(&term[0], &ZERO, &fs.expandedRootsOfUnity[uint64(i)*domainStride])
-	//		if len(ps) > 0 && polyDegree(ps[len(ps)-1]) < 63 {
-	//			ps[len(ps)-1] = fs.mulPolys()ps[len(ps)-1]
-	//		}
-	//	}
-	//}
-	//
-
-
-	return nil, nil // TODO
-}
-
-func truncatePoly(poly []Big) []Big {
-	deg := polyDegree(poly)
-	return poly[:deg+1]
-}
-
-func polyDegree(poly []Big) int {
-	for i := len(poly)-1; i >= 0; i-- {
-		if !equalZero(&poly[i]) {
-			return i
-		}
-	}
-	return -1
-}
+//
+//func truncatePoly(poly []Big) []Big {
+//	deg := polyDegree(poly)
+//	return poly[:deg+1]
+//}
+//
+//func polyDegree(poly []Big) int {
+//	for i := len(poly)-1; i >= 0; i-- {
+//		if !equalZero(&poly[i]) {
+//			return i
+//		}
+//	}
+//	return -1
+//}
 
 // unshift poly, in-place. Multiplies each coeff with 1/shift_factor**i
 func (fs *FFTSettings) ShiftPoly(poly []Big) {
@@ -237,12 +142,21 @@ func (fs *FFTSettings) UnshiftPoly(poly []Big) {
 func (fs *FFTSettings) RecoverPolyFromSamples(samples []*Big, zeroPolyFn ZeroPolyFn) ([]Big, error) {
 	// TODO: using a single additional temporary array, all the FFTs can run in-place.
 
-	zeroVector := make([]bool, len(samples), len(samples))
+	missingIndices := make([]uint64, 0, len(samples))
 	for i, s := range samples {
-		zeroVector[i] = s != nil
+		if s == nil {
+			missingIndices = append(missingIndices, uint64(i))
+		}
 	}
 
-	zeroEval, zeroPoly := zeroPolyFn(zeroVector)
+	zeroEval, zeroPoly := zeroPolyFn(missingIndices)
+
+	for i, s := range samples {
+		if (s == nil) != equalZero(&zeroEval[i]) {
+			panic("bad zero eval")
+		}
+	}
+
 	polyEvaluationsWithZero := make([]Big, len(samples), len(samples))
 	for i, s := range samples {
 		if s == nil {
---
+++ zero_poly.go
@@ -0,0 +1,152 @@
+// Original: https://github.com/ethereum/research/blob/master/polynomial_reconstruction/polynomial_reconstruction.py
+// Changes:
+// - flattened leaf construction,
+// - no aggressive poly truncation
+// - simplified merges
+// - no heap allocations during reduction
+
+package kate
+
+import "fmt"
+
+
+type ZeroPolyFn func (missingIndices []uint64) ([]Big, []Big)
+
+func (fs *FFTSettings) makeZeroPolyMulLeaf(dst []Big, indices []uint64, domainStride uint64) {
+	if len(dst) != len(indices) + 1 {
+		panic(fmt.Sprintf("expected different destination length: %d, got: %d", len(indices)+1, len(dst)))
+	}
+	CopyBigNum(&dst[len(dst)-1], &ONE)
+	var negDi Big
+	for i, v := range indices {
+		subModBig(&negDi, &ZERO, &fs.expandedRootsOfUnity[v*domainStride])
+		CopyBigNum(&dst[i], &negDi)
+		if i > 0 {
+			addModBig(&dst[i], &dst[i], &dst[i-1])
+			for j := i-1; j > 0; j-- {
+				mulModBig(&dst[j], &dst[j], &negDi)
+				addModBig(&dst[j], &dst[j], &dst[j-1])
+			}
+			mulModBig(&dst[0], &dst[0], &negDi)
+		}
+	}
+}
+
+func (fs *FFTSettings) reduceLeaves(scratch []Big, dst []Big, ps [][]Big) {
+	n := uint64(len(dst))
+	if min := uint64(len(ps[0]) * len(ps)); min > n {
+		panic(fmt.Sprintf("expected larger destination length: %d, got: %d", min, n))
+	}
+	if uint64(len(scratch)) < 2*n {
+		panic("not enough scratch space")
+	}
+	// TODO: good to optimize, there's lots of padding
+	pPadded := scratch[:n]
+	prep := func(pi uint64) {
+		p := ps[pi]
+		for i := 0; i < len(p); i++ {
+			CopyBigNum(&pPadded[i], &p[i])
+		}
+		for i := uint64(len(p)); i < n; i++ {
+			CopyBigNum(&pPadded[i], &ZERO)
+		}
+	}
+	mulEvalPs := scratch[n:2*n]
+	// while we're not using dst, use the space for the intermediate results
+	pEval := dst
+	prep(0)
+	if err := fs.InplaceFFT(pPadded, mulEvalPs, false); err != nil {
+		panic(err)
+	}
+	for i := uint64(1); i < uint64(len(ps)); i++ {
+		prep(i)
+		if err := fs.InplaceFFT(pPadded, pEval, false); err != nil {
+			panic(err)
+		}
+		for j := uint64(0); j < n; j++ {
+			mulModBig(&mulEvalPs[j], &mulEvalPs[j], &pEval[j])
+		}
+	}
+	if err := fs.InplaceFFT(mulEvalPs, dst, true); err != nil {
+		panic(err)
+	}
+	return
+}
+
+func (fs *FFTSettings) ZeroPolyViaMultiplication(missingIndices []uint64, length uint64) ([]Big, []Big) {
+	if len(missingIndices) == 0 {
+		panic("no data is missing")
+	}
+	if length > fs.maxWidth {
+		panic("too many ")
+	}
+	if !isPowerOfTwo(length) {
+		panic("length not a power of two")
+	}
+	// just under a power of two, since the leaf gets 1 bigger after building a poly for it
+	perLeaf := 63
+	perLeafPoly := perLeaf + 1
+	leafCount := (len(missingIndices) + perLeaf - 1) / perLeaf
+	n := nextPowOf2(uint64(leafCount * perLeafPoly))
+
+	domainStride := fs.maxWidth / length
+
+	// The assumption here is that if the output is a power of two length, matching the sum of child leaf lengths,
+	// then the space can be reused.
+	out := make([]Big, n, n)
+
+	// Build the leaves.
+
+	// Just the headers, a leaf re-uses the output space.
+	// Combining leaves can be done mostly in-place, using a scratchpad.
+	leaves := make([][]Big, leafCount, leafCount)
+
+	offset := 0
+	outOffset := 0
+	for i := 0; i < leafCount; i++ {
+		end := offset + perLeaf
+		if end < len(missingIndices) {
+			end = len(missingIndices)
+		}
+		leaves[i] = out[outOffset:outOffset+perLeafPoly]
+		fs.makeZeroPolyMulLeaf(leaves[i], missingIndices[offset:end], domainStride)
+		offset += perLeaf
+		outOffset += perLeafPoly
+	}
+
+	// Now reduce all the leaves to a single poly
+
+	// must be a power of 2
+	reductionFactor := 4
+	scratch := make([]Big, n*2, n*2)
+
+	// from bottom to top, start reducing leaves.
+	for len(leaves) > 1 {
+		reducedCount := (len(leaves)+reductionFactor-1)/reductionFactor
+		// all the leaves are the same. Except possibly the last leaf, but that's ok.
+		leafSize := len(leaves[0])
+		for i := 0; i < reducedCount; i++ {
+			reduced := out[i*leafSize : (i+1)*leafSize]
+			start := i*reductionFactor
+			end := start + reductionFactor
+			if end > len(leaves) {
+				end = len(leaves)
+			}
+			if end > start + 1 {
+				// TODO possible optimization if only 2 values are being reduced, instead of 3 or common 4
+
+				// note: reduced overlaps with leaves[start:end]. Perfect overlap if leaves end-start==reduction_factor
+				fs.reduceLeaves(scratch, reduced, leaves[start:end])
+			}
+			leaves[i] = reduced
+		}
+		leaves = leaves[:reducedCount]
+	}
+	zeroPoly := leaves[0]
+	zeroEval, err := fs.FFT(zeroPoly, false)
+	if err != nil {
+		panic(err)
+	}
+
+	return zeroEval, zeroPoly
+}
