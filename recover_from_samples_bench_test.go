c2b441678f928d8ab9312ee66f75d586e55fa667
protolambda <proto@protolambda.com>
2021-01-18 15:32:00 +0100

    zero poly benchmark

--- recover_from_samples_bench_test.go
+++ recover_from_samples_bench_test.go
@@ -1,18 +1,18 @@
 package kate
 
 import (
+	"fmt"
 	"math/rand"
 	"testing"
 )
 
-func BenchmarkFFTSettings_RecoverPolyFromSamples(b *testing.B) {
-	scale := uint8(15)
+func benchRecoverPolyFromSamples(scale uint8, seed int64, b *testing.B) {
 	fs := NewFFTSettings(scale)
 	poly := make([]Big, fs.maxWidth, fs.maxWidth)
 	for i := uint64(0); i < fs.maxWidth/2; i++ {
 		asBig(&poly[i], i)
 	}
-	rng := rand.New(rand.NewSource(1234))
+	rng := rand.New(rand.NewSource(seed))
 	data, _ := fs.FFT(poly, false)
 	samples := make([]*Big, fs.maxWidth, fs.maxWidth)
 	for i := 0; i < len(data); i++ {
@@ -39,5 +39,13 @@ func BenchmarkFFTSettings_RecoverPolyFromSamples(b *testing.B) {
 				b.Fatalf("bad recovered output %d: %s <> %s", i, bigStr(&recovered[i]), bigStr(&data[i]))
 			}
 		}
+	}
+}
+
+func BenchmarkFFTSettings_RecoverPolyFromSamples(b *testing.B) {
+	for scale := uint8(5); scale < 16; scale++ {
+		b.Run(fmt.Sprintf("scale_%d", scale), func(b *testing.B) {
+			benchRecoverPolyFromSamples(scale, int64(scale), b)
+		})
 	}
 }
---
+++ zero_poly_bench_test.go
@@ -0,0 +1,37 @@
+package kate
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+)
+
+func benchZeroPoly(scale uint8, seed int64, b *testing.B) {
+	fs := NewFFTSettings(scale)
+	missing := make([]uint64, fs.maxWidth, fs.maxWidth)
+	for i := uint64(0); i < uint64(len(missing)); i++ {
+		missing[i] = i
+	}
+	rng := rand.New(rand.NewSource(seed))
+	rng.Shuffle(len(missing), func(i, j int) {
+		missing[i], missing[j] = missing[j], missing[i]
+	})
+	// Only consider 50% as missing. Leaves enough FFT computation room.
+	missing = missing[:len(missing)/2]
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		zeroEval, zeroPoly := fs.ZeroPolyViaMultiplication(missing, fs.maxWidth)
+		if len(zeroEval) != len(zeroPoly) {
+			panic("sanity check failed, length mismatch")
+		}
+	}
+}
+
+func BenchmarkFFTSettings_ZeroPolyViaMultiplication(b *testing.B) {
+	for scale := uint8(5); scale < 16; scale++ {
+		b.Run(fmt.Sprintf("scale_%d", scale), func(b *testing.B) {
+			benchZeroPoly(scale, int64(scale), b)
+		})
+	}
+}
