f7993c8f97c85b357d509bb4c2c040e78df21c95
Guillaume Ballet <gballet@gmail.com>
2021-03-03 14:25:13 +0100

    Setup helper function that also provides s1 in evaluation form

--- setup.go
+++ setup.go
@@ -22,3 +22,26 @@ func GenerateTestingSetup(secret string, n uint64) ([]bls.G1Point, []bls.G2Point
 	return s1Out, s2Out
 }
 
+// GenerateTestingSetupWithLagrange creates a setup of n values from the given secret,
+// along with the expression of s1 in evaluation form. **for testing purposes only**
+func GenerateTestingSetupWithLagrange(secret string, n uint64, fftCfg *FFTSettings) ([]bls.G1Point, []bls.G2Point, []bls.G1Point, error) {
+	var s bls.Fr
+	bls.SetFr(&s, secret)
+
+	var sPow bls.Fr
+	bls.CopyFr(&sPow, &bls.ONE)
+
+	s1Out := make([]bls.G1Point, n, n)
+	s2Out := make([]bls.G2Point, n, n)
+	for i := uint64(0); i < n; i++ {
+		bls.MulG1(&s1Out[i], &bls.GenG1, &sPow)
+		bls.MulG2(&s2Out[i], &bls.GenG2, &sPow)
+		var tmp bls.Fr
+		bls.CopyFr(&tmp, &sPow)
+		bls.MulModFr(&sPow, &tmp, &s)
+	}
+
+	s1Lagrange, err := fftCfg.FFTG1(s1Out, true)
+
+	return s1Out, s2Out, s1Lagrange, err
+}
